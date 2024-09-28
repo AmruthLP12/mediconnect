63b987fe45b3ac7ea49dec1d82946f3ad79e75a4
Amruth L P <amruthlp12@gmail.com>
2024-09-28 13:53:47 +0530

    success page done

--- lib/actions/appointment.actions.ts
+++ lib/actions/appointment.actions.ts
@@ -1,14 +1,16 @@
-'use server'
+"use server";
 
 import { ID } from "node-appwrite";
 import {
-    APPOINTMENT_COLLECTION_ID,
-    DATABASE_ID,
-    databases
+  APPOINTMENT_COLLECTION_ID,
+  DATABASE_ID,
+  databases,
 } from "../appwrite.config";
 import { parseStringify } from "../utils";
 
-export const CreateAppointment = async (appointment : CreateAppointmentParams) => {
+export const CreateAppointment = async (
+  appointment: CreateAppointmentParams
+) => {
   try {
     const newAppointment = await databases.createDocument(
       DATABASE_ID!,
@@ -18,6 +20,20 @@ export const CreateAppointment = async (appointment : CreateAppointmentParams) =
     );
 
     return parseStringify(newAppointment);
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+export const getAppointment = async (appointmentId: string) => {
+  try {
+    const appointment = await databases.getDocument(
+      DATABASE_ID!,
+      APPOINTMENT_COLLECTION_ID!,
+      appointmentId
+    );
+
+    return parseStringify(appointment);
   } catch (error) {
     console.log(error);
   }
