820dae19a2a204282329f00f872b8775da9aea00
Amruth L P <amruthlp12@gmail.com>
2024-09-30 16:26:48 +0530

    completed

--- lib/actions/appointment.actions.ts
+++ lib/actions/appointment.actions.ts
@@ -5,8 +5,9 @@ import {
   APPOINTMENT_COLLECTION_ID,
   DATABASE_ID,
   databases,
+  messaging,
 } from "../appwrite.config";
-import { parseStringify } from "../utils";
+import { formatDateTime, parseStringify } from "../utils";
 import { Appointment } from "@/types/appwrite.types";
 import { revalidatePath } from "next/cache";
 
@@ -46,9 +47,8 @@ export const getRecentAppointmentList = async () => {
     const appointments = await databases.listDocuments(
       DATABASE_ID!,
       APPOINTMENT_COLLECTION_ID!,
-       [Query.orderDesc("$createdAt"), Query.limit(100)]
+      [Query.orderDesc("$createdAt"), Query.limit(100)]
     );
-
 
     const initialCounts = {
       scheduledCount: 0,
@@ -96,14 +96,42 @@ export const updateAppointment = async ({
       appointment
     );
 
+    console.log(appointment)
+
     if (!updatedAppointment) {
       throw new Error("Appointment not found");
     }
 
-    // TODO SMS notification
+    const smsMessage = `
+    Hi, it's MediConnect. 
+    ${
+      type === "schedule"
+        ? `Your appointment has been schedule for ${formatDateTime(
+            appointment.schedule!
+          ).dateTime} with Dr. ${appointment.primaryPhysician}`
+        : `We regret to inform you that appointment has been cancelled for the following reason : ${appointment.cancellationReason}`
+    }
+    `;
+
+    await sendSMSNotification(userId, smsMessage);
 
     revalidatePath("/admin");
     return parseStringify(updatedAppointment);
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+export const sendSMSNotification = async (userId: string, content: string) => {
+  try {
+    const message = await messaging.createSms(
+      ID.unique(),
+      content,
+      [],
+      [userId]
+    );
+
+    return parseStringify(message);
   } catch (error) {
     console.log(error);
   }
